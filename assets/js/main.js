fd61c4260208cff8975cd5ca3c742a322115ab8f
StepanGG2011a <145209664+StepanGG2011a@users.noreply.github.com>
2024-04-13 19:01:18 +0300

    выгрузка

---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+let burger = document.querySelector('.burger');
+let mobile_menu = document.querySelector('#mobile-menu');
+let karz = document.querySelector('.karz');
+let karz_menu = document.querySelector('#karz-menu')
+let cross = document.querySelector('.krestic')
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  mobile_menu.classList.toggle('active');
+});
+
+cross.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  mobile_menu.classList.toggle('active');
+});
+
+karz.addEventListener('click', () => {
+  karz.classList.toggle('active');
+  karz_menu.classList.toggle('active');
+});
+
+cross.addEventListener('click', () => {
+  karz.classList.toggle('active');
+  karz_menu.classList.toggle('active');
+});
+
+const toggleContentByTab = () => {
+
+  let tabs = document.querySelectorAll('.products-block__tab');
+  let contents = document.querySelectorAll('.products-block__wrapper');
+  tabs.forEach((tab, index) => {
+    console.log(tab);
+    tab.addEventListener('click', () => {
+      tabs.forEach((tab_2) => {
+        tab_2.classList.remove('active');
+      })
+      contents.forEach((content) => {
+        content.classList.remove('active');
+      })
+      tab.classList.add('active');
+      contents[index].classList.add('active');
+    })
+  })
+}
+
+toggleContentByTab();
